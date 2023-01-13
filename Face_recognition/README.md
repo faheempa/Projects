7336a3908c2311f253383368a462cf950f2bcada
faheemfahi <faheempa788@gmail.com>
2023-01-14 00:38:04 +0530

    updated

---
+++ Face_recognition/README.md
@@ -0,0 +1,6 @@
+# Face Recognition
+
+There are 2 folder in this project, known and unknown.
+As the name suggest, known folder contain pictures of people who we know and unknown folder contain pictures of people who we do not know.
+The program analyzes the pics of known people and learn their facial features.
+Then the program analyzes the unkown folder and recognise the person.
---
+++ Face_recognition/main.py
@@ -0,0 +1,36 @@
+# This project was done in google colab, because face recognition module is not getting installed in my system
+# link to google colab page is given below
+# https://drive.google.com/file/d/17fpoPlgqC7m4q7zTfRMiCx0qXFhW7LAt/view?usp=sharing
+
+import face_recognition
+import cv2
+import os
+from google.colab.patches import cv2_imshow
+
+def read_img(path, width = 320):
+  img = cv2.imread(path)
+  (h, w) = img.shape[:2]
+  height = int(h * (width / float(w)))
+  return cv2.resize(img, (width, height))
+
+known = []
+names = []
+known_dir = "known"
+for file in os.listdir(known_dir):
+  img = read_img(f"{known_dir}/{file}")
+  known.append(face_recognition.face_encodings(img)[0])
+  names.append(file.split('.')[0])
+
+unknown_dir = "unknown"
+for file in os.listdir(unknown_dir):
+  img = read_img(f"{unknown_dir}/{file}")
+  unknown = face_recognition.face_encodings(img)[0]
+  results = face_recognition.compare_faces(known, unknown)
+  for i in range(len(results)):
+    if results[i]:
+      name = names[i]
+      (top,right,bottom,left) = face_recognition.face_locations(img)[0]
+      cv2.rectangle(img, (left,top), (right,bottom), (0,0,255), 2)
+      cv2.putText(img, name, (left+2,bottom+20), cv2.FONT_HERSHEY_PLAIN, 1,
+                  (255,255,255), 1)
+      cv2_imshow(img)
